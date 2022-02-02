d12a81f597515f59172548a9fb6f3ea1aebe079d
tayyab94 <tayyab.bhatti30@gmail.com>
2022-02-03 01:25:06 +0500

    First Commit

---
+++ UserService/Database/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Database.Entities;
+
+namespace UserService.Database
+{
+    public class DatabaseContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-8VTLERK\\SQLEXPRESS; database=microserviceDB; Trusted_Connection=true;MultipleActiveResultSets=true");
+        }
+    }
+}
